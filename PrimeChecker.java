bd683d33d4984e3c9333aabf6c685124dc8ea5bf
CS106A <>
2019-01-28 15:43:17 -0800

    {"types":"C","runs":0}

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -8,10 +8,14 @@ import acm.program.*;
 
 public class PrimeChecker extends ConsoleProgram {
 	
-	int[] testCases = {2, 3, 8, 37,  42, 87, 382, 1019};
+	int[] testCases = {2, 3, 8, 37,  42, 87, 361, 382, 729, 1019};
+	bool[] answers = {true, true, false, true, false, false, false, false, false, true};
 	
 	public void run() {	
-		
+		for (int i = 0; i < testCases.length; i++) {
+			int testCase = testCases[i];
+			
+		}
 	}
 	
 	/**
