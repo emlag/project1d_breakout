0c4415afc183a661ee3c2380bea5772910a990ef
CS106A <>
2019-01-28 15:40:54 -0800

    {"types":"C","runs":0}

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -1,19 +1,24 @@
 /*
- * File: MouseReporter.java
+ * File: PrimeChecker.java
  * -----------------------------
- * Output the location of the mouse to a label on the
- * screen. Change the color of the label to red when
- * the mouse touches it.
+ * Checks whether a series of numbers are prime
  */
 
 import acm.program.*;
 
 public class PrimeChecker extends ConsoleProgram {
 	
+	int[] testCases = {2, 3, 8, 37,  42, 87, 382, 1019};
+	
 	public void run() {	
 		
 	}
 	
+	/**
+	 * 
+	 * @param n a positive integer greater than 1
+	 * @return true if n is prime and false otherwise
+	 */
 	private boolean isPrime(int n) {
 		
 		return true; // this is only here so the program compiles
