8f5f02ba983f5dbb0a863bbff31bc9c5cb9ffcc8
CS106A <>
2019-01-28 15:37:13 -0800

    {"types":"C","runs":0}

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -1,4 +1,22 @@
+/*
+ * File: MouseReporter.java
+ * -----------------------------
+ * Output the location of the mouse to a label on the
+ * screen. Change the color of the label to red when
+ * the mouse touches it.
+ */
 
-public class PrimeChecker {
+import acm.program.*;
+
+public class PrimeChecker extends ConsoleProgram {
+	
+	public void run() {	
+		
+	}
+	
+	private boolean isPrime(int n) {
+		
+	}
+
 
 }
