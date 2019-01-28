19d30f4d6ed13da50e9a788511f63ca42565a17d
CS106A <>
2019-01-28 15:52:20 -0800

    {"types":"C","runs":4}

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -11,6 +11,13 @@ public class PrimeChecker extends ConsoleProgram {
 	int[] testCases = {2, 3, 8, 37,  42, 87, 361, 382, 729, 1019};
 	boolean[] answers = {true, true, false, true, false, false, false, false, false, true};
 	
+	/**
+	 * Don't modify this method! You only need to edit the 
+	 * isPrime method so that it works correctly. 
+	 * 
+	 * The run method just tests your method against a variety
+	 * of numbers to make sure that it's behaving as expected.
+	 */
 	public void run() {	
 		for (int i = 0; i < testCases.length; i++) {
 			int testCase = testCases[i];
